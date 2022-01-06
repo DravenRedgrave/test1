a9579b4fe609ec206881c23ca104f00be3962944
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2022-01-06 21:42:56 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Wave from 'react-wavify'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Home from './Home';
+import {CopyToClipboard} from 'react-copy-to-clipboard'
+import zero from './assets/pngegg.png'
 
 function App() {
+  const [value, setValue] = useState('')
+  const [copie,setCopie] = useState(false)
   return (
     <>
+  <center>
+  <input
+      value={value}
+      onChange={({target:{value}}) => setValue(value)}
+    />
+    <CopyToClipboard text={value} onCopy={()=> setCopie(true)}> 
+      <button>Copy to clipboard</button>
+    </CopyToClipboard>
+    {copie ? <span style={{color:'green'}}>Copy</span>:null}
+  </center>
+  <div className='container mt-3'>
+    <div className='row'>
+      <div className='col-md-5'>
+        Signup
+      </div>
+      <div className='col-md-7'>
+        <img className='img-fluid w-50' src={zero} alt='zeroTwo'/>
+      </div>
+    </div>
+  </div>
     <h1 className='text-center text-info mt-3'>Password</h1>
     <Home/>
     <div className='App'>
